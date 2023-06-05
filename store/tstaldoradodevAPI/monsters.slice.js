76ffa2a35180171f9b15699d407cd9fc4bb4144c
Crowdbotics <engineering@crowdbotics.com>
2023-06-05 14:35:30 +0000

    Update store.

---
+++ store/tstaldoradodevAPI/monsters.slice.js
@@ -0,0 +1,172 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
+import { apiService } from "./api"
+export const api_v1_monster_list = createAsyncThunk(
+  "monsters/api_v1_monster_list",
+  async payload => {
+    const response = await apiService.api_v1_monster_list(payload)
+    return response.data
+  }
+)
+export const api_v1_monster_create = createAsyncThunk(
+  "monsters/api_v1_monster_create",
+  async payload => {
+    const response = await apiService.api_v1_monster_create(payload)
+    return response.data
+  }
+)
+export const api_v1_monster_retrieve = createAsyncThunk(
+  "monsters/api_v1_monster_retrieve",
+  async payload => {
+    const response = await apiService.api_v1_monster_retrieve(payload)
+    return response.data
+  }
+)
+export const api_v1_monster_update = createAsyncThunk(
+  "monsters/api_v1_monster_update",
+  async payload => {
+    const response = await apiService.api_v1_monster_update(payload)
+    return response.data
+  }
+)
+export const api_v1_monster_partial_update = createAsyncThunk(
+  "monsters/api_v1_monster_partial_update",
+  async payload => {
+    const response = await apiService.api_v1_monster_partial_update(payload)
+    return response.data
+  }
+)
+export const api_v1_monster_destroy = createAsyncThunk(
+  "monsters/api_v1_monster_destroy",
+  async payload => {
+    const response = await apiService.api_v1_monster_destroy(payload)
+    return response.data
+  }
+)
+const initialState = { entities: [], api: { loading: "idle", error: null } }
+const monstersSlice = createSlice({
+  name: "monsters",
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [api_v1_monster_list.pending]: (state, action) => {
+      if (state.api.loading === "idle") {
+        state.api.loading = "pending"
+      }
+    },
+    [api_v1_monster_list.fulfilled]: (state, action) => {
+      if (state.api.loading === "pending") {
+        state.entities = action.payload
+        state.api.loading = "idle"
+      }
+    },
+    [api_v1_monster_list.rejected]: (state, action) => {
+      if (state.api.loading === "pending") {
+        state.api.error = action.error
+        state.api.loading = "idle"
+      }
+    },
+    [api_v1_monster_create.pending]: (state, action) => {
+      if (state.api.loading === "idle") {
+        state.api.loading = "pending"
+      }
+    },
+    [api_v1_monster_create.fulfilled]: (state, action) => {
+      if (state.api.loading === "pending") {
+        state.entities.push(action.payload)
+        state.api.loading = "idle"
+      }
+    },
+    [api_v1_monster_create.rejected]: (state, action) => {
+      if (state.api.loading === "pending") {
+        state.api.error = action.error
+        state.api.loading = "idle"
+      }
+    },
+    [api_v1_monster_retrieve.pending]: (state, action) => {
+      if (state.api.loading === "idle") {
+        state.api.loading = "pending"
+      }
+    },
+    [api_v1_monster_retrieve.fulfilled]: (state, action) => {
+      if (state.api.loading === "pending") {
+        state.entities = [
+          ...state.entities.filter(record => record.id !== action.payload.id),
+          action.payload
+        ]
+        state.api.loading = "idle"
+      }
+    },
+    [api_v1_monster_retrieve.rejected]: (state, action) => {
+      if (state.api.loading === "pending") {
+        state.api.error = action.error
+        state.api.loading = "idle"
+      }
+    },
+    [api_v1_monster_update.pending]: (state, action) => {
+      if (state.api.loading === "idle") {
+        state.api.loading = "pending"
+      }
+    },
+    [api_v1_monster_update.fulfilled]: (state, action) => {
+      if (state.api.loading === "pending") {
+        state.entities = state.entities.map(record =>
+          record.id === action.payload.id ? action.payload : record
+        )
+        state.api.loading = "idle"
+      }
+    },
+    [api_v1_monster_update.rejected]: (state, action) => {
+      if (state.api.loading === "pending") {
+        state.api.error = action.error
+        state.api.loading = "idle"
+      }
+    },
+    [api_v1_monster_partial_update.pending]: (state, action) => {
+      if (state.api.loading === "idle") {
+        state.api.loading = "pending"
+      }
+    },
+    [api_v1_monster_partial_update.fulfilled]: (state, action) => {
+      if (state.api.loading === "pending") {
+        state.entities = state.entities.map(record =>
+          record.id === action.payload.id ? action.payload : record
+        )
+        state.api.loading = "idle"
+      }
+    },
+    [api_v1_monster_partial_update.rejected]: (state, action) => {
+      if (state.api.loading === "pending") {
+        state.api.error = action.error
+        state.api.loading = "idle"
+      }
+    },
+    [api_v1_monster_destroy.pending]: (state, action) => {
+      if (state.api.loading === "idle") {
+        state.api.loading = "pending"
+      }
+    },
+    [api_v1_monster_destroy.fulfilled]: (state, action) => {
+      if (state.api.loading === "pending") {
+        state.entities = state.entities.filter(
+          record => record.id !== action.meta.arg?.id
+        )
+        state.api.loading = "idle"
+      }
+    },
+    [api_v1_monster_destroy.rejected]: (state, action) => {
+      if (state.api.loading === "pending") {
+        state.api.error = action.error
+        state.api.loading = "idle"
+      }
+    }
+  }
+})
+export default {
+  api_v1_monster_list,
+  api_v1_monster_create,
+  api_v1_monster_retrieve,
+  api_v1_monster_update,
+  api_v1_monster_partial_update,
+  api_v1_monster_destroy,
+  slice: monstersSlice
+}
