92be5f4994889877506831c126f0c36143048788
crowdbotics-dev <engineering@crowdbotics.com>
2023-06-05 06:27:20 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Monster
+admin.site.register(Monster)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,5 @@
+from django.conf import settings
 from django.db import models
+class Monster(models.Model):
+    'Generated Model'
+    kgf = models.BigIntegerField()
